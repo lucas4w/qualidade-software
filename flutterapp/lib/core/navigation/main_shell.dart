13ed5ed08b47438169f5751a7ae30162053a6495
Cirrus CI <support@cirruslabs.org>
2026-06-01 03:11:57 +0000

    fix: remove unused imports

--- flutterapp/lib/core/navigation/main_shell.dart
+++ flutterapp/lib/core/navigation/main_shell.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp/core/theme/pallete.dart';
 import 'package:flutterapp/features/notificacoes/services/notificacoes_service.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class MainShell extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
